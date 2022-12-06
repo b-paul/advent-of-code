bbe6275dc12abd95db7a2fcd53d946894b905e5f
bpaul <bpaul@bpaul.xyz>
2022-12-06 17:21:59 +1000

    day 6 yay!! (code today was really fast!! wow!!

--- src/year_2022/mod.rs
+++ src/year_2022/mod.rs
@@ -2,7 +2,7 @@ use crate::day::Day;
 use crate::make_day;
 
 // Amount of days that have problems so far this year
-pub const DAYS_NUM: usize = 5;
+pub const DAYS_NUM: usize = 6;
 
 pub const DAYS: [Day; DAYS_NUM] = [
     make_day!(day1),
@@ -10,6 +10,7 @@ pub const DAYS: [Day; DAYS_NUM] = [
     make_day!(day3),
     make_day!(day4),
     make_day!(day5),
+    make_day!(day6),
 ];
 
 mod day1;
@@ -17,3 +18,4 @@ mod day2;
 mod day3;
 mod day4;
 mod day5;
+mod day6;
