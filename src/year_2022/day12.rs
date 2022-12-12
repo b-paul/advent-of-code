2b1e9177864f7d5573ad651a9a7b2544802895a5
Benjamin Paul <bpaul@bpaul.xyz>
2022-12-12 15:27:15 +1000

    day 12 890th!!

---
+++ src/year_2022/day12.rs
@@ -0,0 +1,154 @@
+use std::collections::VecDeque;
+use std::collections::HashSet;
+
+pub fn part_1(input: String) -> String {
+    let mut startpos = (0,0);
+    let mut bestpos = (0,0);
+    let mut grid = vec![];
+    for (y, iline) in input.lines().enumerate() {
+        let mut line = vec![];
+        for (x, byte) in iline.bytes().enumerate() {
+            if byte == 10 {
+                continue;
+            }
+
+            if byte == b'S' {
+                startpos = (x, y);
+                line.push(0);
+            } else if byte == b'E' {
+                bestpos = (x, y);
+                line.push(25);
+            } else {
+                line.push((byte - b'a') as isize);
+            }
+        }
+        grid.push(line);
+    }
+
+    let mut stack = VecDeque::new();
+    let mut visited = HashSet::new();
+    stack.push_back((startpos, 0));
+
+    let mut bdepth = 0;
+    while let Some(((x, y), depth)) = stack.pop_front() {
+        if visited.contains(&(x, y)) {
+            continue;
+        }
+
+        if (x, y) == bestpos {
+            bdepth = depth;
+            break;
+        }
+
+        visited.insert((x, y));
+
+        if x != 0 {
+            if grid[y][x] - grid[y][x-1] >= -1 {
+                stack.push_back(((x-1, y), depth+1));
+            }
+        }
+        if x != (grid[0].len()-1) {
+            if grid[y][x] - grid[y][x+1] >= -1 {
+                stack.push_back(((x+1, y), depth+1));
+            }
+        }
+        if y != 0 {
+            if grid[y][x] - grid[y-1][x] >= -1 {
+                stack.push_back(((x, y-1), depth+1));
+            }
+        }
+        if y != (grid.len()-1) {
+            if grid[y][x] - grid[y+1][x] >= -1 {
+                stack.push_back(((x, y+1), depth+1));
+            }
+        }
+    };
+
+    bdepth.to_string()
+}
+
+#[test]
+fn testp1() {
+    assert_eq!(
+        part_1("Sabqponm
+abcryxxl
+accszExk
+acctuvwj
+abdefghi".to_string()), 31.to_string()
+        );
+}
+
+pub fn part_2(input: String) -> String {
+    let mut bestpos = (0,0);
+    let mut grid = vec![];
+    for (y, iline) in input.lines().enumerate() {
+        let mut line = vec![];
+        for (x, byte) in iline.bytes().enumerate() {
+            if byte == 10 {
+                continue;
+            }
+
+            if byte == b'S' {
+                line.push(0);
+            } else if byte == b'E' {
+                bestpos = (x, y);
+                line.push(25);
+            } else {
+                line.push((byte - b'a') as isize);
+            }
+        }
+        grid.push(line);
+    }
+
+    let mut stack = VecDeque::new();
+    let mut visited = HashSet::new();
+    stack.push_back((bestpos, 0));
+
+    let mut bdepth = 0;
+    while let Some(((x, y), depth)) = stack.pop_front() {
+        if visited.contains(&(x, y)) {
+            continue;
+        }
+
+        if grid[y][x] == 0 {
+            bdepth = depth;
+            break;
+        }
+
+        visited.insert((x, y));
+
+        if x != 0 {
+            if grid[y][x] - grid[y][x-1] <= 1 {
+                stack.push_back(((x-1, y), depth+1));
+            }
+        }
+        if x != (grid[0].len()-1) {
+            if grid[y][x] - grid[y][x+1] <= 1 {
+                stack.push_back(((x+1, y), depth+1));
+            }
+        }
+        if y != 0 {
+            if grid[y][x] - grid[y-1][x] <= 1 {
+                stack.push_back(((x, y-1), depth+1));
+            }
+        }
+        if y != (grid.len()-1) {
+            if grid[y][x] - grid[y+1][x] <= 1 {
+                stack.push_back(((x, y+1), depth+1));
+            }
+        }
+    };
+
+    bdepth.to_string()
+}
+
+#[test]
+fn testp2() {
+    assert_eq!(
+        part_2("Sabqponm
+abcryxxl
+accszExk
+acctuvwj
+abdefghi".to_string()), 29.to_string()
+        );
+}
