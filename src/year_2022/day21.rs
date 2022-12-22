cf33d1ccfe95c7a5cb54e348f16582443ab2d1b5
Benjamin Paul <bpaul@bpaul.xyz>
2022-12-22 14:48:59 +1000

    Pretend i've done days 17-21 so that i can do day 22

---
+++ src/year_2022/day21.rs
@@ -0,0 +1,13 @@
+pub fn part_1(input: String) -> String {
+    "Will do this later".to_string()
+}
+
+#[test]
+fn testp1() {}
+
+pub fn part_2(input: String) -> String {
+    "Will do this later".to_string()
+}
+
+#[test]
+fn testp2() {}
