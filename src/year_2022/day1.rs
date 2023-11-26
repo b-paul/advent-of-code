a7f4f29ea2c711d0418d99fd40a5967a513c4bf1
bpaul <bpaul@bpaul.xyz>
2023-11-27 04:14:30 +1000

    Clean up 2022 day1

--- src/year_2022/day1.rs
+++ src/year_2022/day1.rs
@@ -1,48 +1,25 @@
-fn elf_callories(str: &str) -> u64 {
-    let mut total = 0;
+use itertools::Itertools;
 
-    for line in str.split('\n') {
-        let num: u64 = line.parse().unwrap();
-
-        total += num;
-    }
-
-    total
+fn callories(str: &str) -> u64 {
+    str.lines().filter_map(|l| l.parse::<u64>().ok()).sum()
 }
 
 pub fn part_1(input: String) -> String {
-    let mut str = input.chars();
-    str.next_back();
-    let elves = str.as_str().split("\n\n");
-
-    let most_callories = elves.into_iter().map(elf_callories).max().unwrap();
-
-    most_callories.to_string()
+    input
+        .split("\n\n")
+        .map(callories)
+        .max()
+        .unwrap_or_default()
+        .to_string()
 }
 
 pub fn part_2(input: String) -> String {
-    let mut str = input.chars();
-    str.next_back();
-    let elves = str.as_str().split("\n\n");
-
-    // elves with the most callories ranked
-    let mut elf1 = 0;
-    let mut elf2 = 0;
-    let mut elf3 = 0;
-
-    for elf in elves {
-        let elf_callories = elf_callories(elf);
-        if elf_callories > elf1 {
-            elf3 = elf2;
-            elf2 = elf1;
-            elf1 = elf_callories;
-        } else if elf_callories > elf2 {
-            elf2 = elf_callories;
-            elf3 = elf2;
-        } else if elf_callories > elf3 {
-            elf3 = elf_callories;
-        }
-    }
-
-    (elf1 + elf2 + elf3).to_string()
+    input
+        .split("\n\n")
+        .map(callories)
+        .sorted()
+        .rev()
+        .take(3)
+        .sum::<u64>()
+        .to_string()
 }
