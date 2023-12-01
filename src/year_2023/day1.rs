f1ceab0c3c1384cce5154f4b47d453c4a34dfc90
bpaul <bpaul@bpaul.xyz>
2023-12-01 15:43:18 +1000

    2023 day 1 terrible code

--- src/year_2023/day1.rs
+++ src/year_2023/day1.rs
@@ -1,7 +1,54 @@
-pub fn part_1(_input: &str) -> impl std::fmt::Display {
-
+pub fn part_1(input: &str) -> impl std::fmt::Display {
+    input.lines()
+        .map(|l| {
+            let mut n = 0;
+            for c in l.chars() {
+                if c.is_digit(10) {
+                    n += c.to_digit(10).unwrap();
+                    break;
+                }
+            }
+            n *= 10;
+            for c in l.chars().rev() {
+                if c.is_digit(10) {
+                    n += c.to_digit(10).unwrap();
+                    break;
+                }
+            }
+            n
+        }).sum::<u32>()
 }
 
-pub fn part_2(_input: &str) -> impl std::fmt::Display {
-    "todo"
+pub fn part_2(input: &str) -> impl std::fmt::Display {
+    input.lines()
+        .map(|l| {
+            let nums = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+            let mut n = 0;
+            'end: for (i, c) in l.chars().enumerate() {
+                if c.is_digit(10) {
+                    n += c.to_digit(10).unwrap();
+                    break;
+                }
+                for (j, num) in nums.iter().enumerate() {
+                    if l[i..].starts_with(num) {
+                        n += j as u32 + 1;
+                        break 'end;
+                    }
+                }
+            }
+            n *= 10;
+            'end: for (i, c) in l.chars().rev().enumerate() {
+                if c.is_digit(10) {
+                    n += c.to_digit(10).unwrap();
+                    break;
+                }
+                for (j, num) in nums.iter().enumerate() {
+                    if l[(l.len() - 1 - i)..].starts_with(num) {
+                        n += j as u32 + 1;
+                        break 'end;
+                    }
+                }
+            }
+            n
+        }).sum::<u32>()
 }
