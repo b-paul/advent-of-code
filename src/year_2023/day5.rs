0096ae747abd46d2f261a30cdfb73bec68b0b669
benjamin paul <bpaul@bpaul.xyz>
2023-12-05 15:46:23 +1000

    WHAT I GOT TOP 100 (for part 1 391th:( for part 2)???? day5 2023

--- src/year_2023/day5.rs
+++ src/year_2023/day5.rs
@@ -1,7 +1,80 @@
-pub fn part_1(_input: &str) -> impl std::fmt::Display {
-    "todo"
+pub fn part_1(input: &str) -> impl std::fmt::Display {
+    let data = input.split("\n\n").collect::<Vec<_>>();
+
+    let mut seeds = data[0].split(' ').skip(1).filter_map(|n| n.parse::<usize>().ok()).collect::<Vec<_>>();
+
+    for di in 1..8 {
+        let data = data[di];
+        let data = data.lines().skip(1);
+        let ranges = data.map(|l| {
+            let xs = l.split(' ').filter_map(|n| n.parse::<usize>().ok()).collect::<Vec<_>>();
+            (xs[0], xs[1], xs[2])
+        }).collect::<Vec<_>>();
+        'next: for seed in seeds.iter_mut() {
+            for (dest, source, len) in ranges.iter() {
+                if *seed >= *source && *seed < *source + *len {
+                    *seed = *seed + dest - source;
+                    continue 'next;
+                }
+            }
+        }
+    }
+
+    seeds.into_iter().min().unwrap()
 }
 
-pub fn part_2(_input: &str) -> impl std::fmt::Display {
-    "todo"
+pub fn part_2(input: &str) -> impl std::fmt::Display {
+    let data = input.split("\n\n").collect::<Vec<_>>();
+
+    let seed_data = data[0].split(' ').skip(1).filter_map(|n| n.parse::<usize>().ok()).collect::<Vec<_>>();
+    let mut seeds = Vec::new();
+    for i in 0..seed_data.len()/2 {
+        let start = seed_data[2*i];
+        let len = seed_data[2*i + 1];
+        seeds.push(start..start + len);
+    }
+
+    for di in 1..8 {
+        let mut new_seeds = Vec::new();
+
+        let data = data[di];
+        let data = data.lines().skip(1);
+        let ranges = data.map(|l| {
+            let xs = l.split(' ').filter_map(|n| n.parse::<usize>().ok()).collect::<Vec<_>>();
+            (xs[0], xs[1], xs[2])
+        }).collect::<Vec<_>>();
+        'next: while let Some(seed) = seeds.pop() {
+            for (dest, source, len) in ranges.iter() {
+                if seed.start >= *source && seed.start < source + len {
+                    let new_len = (*len).min(seed.len() as usize) as usize;
+                    if new_len == seed.len() {
+                        let start = seed.start + dest - source;
+                        let end = seed.end + dest - source;
+                        new_seeds.push(start..end);
+                    } else {
+                        seeds.push((seed.start + *len)..seed.end);
+
+                        let start = seed.start + dest - source;
+                        let end = start + len;
+                        new_seeds.push(start..end);
+                    }
+                    continue 'next;
+                } else if seed.end > *source && seed.end < source + len {
+                    let len = seed.end - source;
+                    seeds.push(seed.start..*source);
+                    new_seeds.push(*dest..*dest+len);
+                    continue 'next;
+                } else if seed.start < *source && seed.end >= source + len {
+                    seeds.push(seed.start..*source);
+                    seeds.push(*source + *len..seed.end);
+                    new_seeds.push(*dest..*dest+len);
+                    continue 'next;
+                }
+            }
+            new_seeds.push(seed);
+        }
+
+        seeds = new_seeds.clone();
+    }
+    seeds.iter().map(|r| r.start).min().unwrap()
 }
