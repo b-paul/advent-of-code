8d7666ae05c18319c8ab50d707cc10fcd9d20b5f
bpaul <bpaul@bpaul.xyz>
2023-12-06 22:01:14 +1000

    Uhhhh use the quadratic formula or something day6

--- src/year_2023/day6.rs
+++ src/year_2023/day6.rs
@@ -1,31 +1,55 @@
-use itertools::Itertools;
-
 pub fn part_1(input: &str) -> impl std::fmt::Display {
-    let lines = input.lines().map(|l| { l.split_ascii_whitespace().collect_vec() }).collect_vec();
-    let times = lines[0][1..].iter().map(|n| n.parse::<u32>().unwrap()).collect_vec();
-    let dists = lines[1][1..].iter().map(|n| n.parse::<u32>().unwrap()).collect_vec();
+    let times = input[11..37]
+        .split_ascii_whitespace()
+        .map(|n| n.parse::<f64>().unwrap());
+    let dists = input[48..]
+        .split_ascii_whitespace()
+        .map(|n| n.parse::<f64>().unwrap());
 
-    times.into_iter().zip(dists.into_iter()).map(|(t, d)| {
-        let mut count = 0;
-        for b in 0..t {
-            if b * (t - b) > d {
-                count += 1;
-            }
-        }
-        count
-    }).product::<u32>()
+    times
+        .zip(dists)
+        .map(|(t, d)| {
+            let high = ((t + (t * t - d * 4.).sqrt()) / 2. - 0.01).floor();
+            let low = ((t - (t * t - d * 4.).sqrt()) / 2. + 0.01).ceil();
+            high - low + 1.
+        })
+        .product::<f64>()
 }
 
 pub fn part_2(input: &str) -> impl std::fmt::Display {
-    let lines = input.lines().map(|l| { l.chars().skip(11).collect::<String>().split_ascii_whitespace().join("") }).collect_vec();
-    let t = lines[0].parse::<u64>().unwrap();
-    let d = lines[1].parse::<u64>().unwrap();
+    let mut lines = input.as_bytes().chunks(37).map(|l| {
+        l.iter()
+            .skip(11)
+            .filter(|b| b'0' <= **b && **b <= b'9')
+            .map(|b| (b - b'0') as u64)
+            .fold(0, |a, n| a * 10 + n) as f64
+    });
+    let t = lines.next().unwrap();
+    let d = lines.next().unwrap();
 
-    let mut count = 0;
-    for b in 0..t {
-        if b * (t - b) > d {
-            count += 1;
-        }
+    let high = ((t + (t * t - d * 4.).sqrt()) / 2. - 0.01).floor();
+    let low = ((t - (t * t - d * 4.).sqrt()) / 2. + 0.01).ceil();
+    high - low + 1.
+}
+
+mod benches {
+    use crate::get_input;
+    use crate::year_2023::day6::*;
+    use test::{black_box, Bencher};
+
+    #[bench]
+    fn part1_normal(b: &mut Bencher) {
+        let input = &get_input(2023, 6).unwrap();
+        b.iter(|| {
+            black_box(part_1(input));
+        })
     }
-    count
+
+    #[bench]
+    fn part2_normal(b: &mut Bencher) {
+        let input = &get_input(2023, 6).unwrap();
+        b.iter(|| {
+            black_box(part_2(input));
+        })
+    }
 }
