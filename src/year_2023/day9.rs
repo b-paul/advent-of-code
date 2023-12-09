b3e2444e5dbef80ae06b3f38f956f02c3564569a
Benjamin Paul <bpaul@bpaul.xyz>
2023-12-09 15:30:50 +1000

    Zip and .last 2023d9

--- src/year_2023/day9.rs
+++ src/year_2023/day9.rs
@@ -1,30 +1,42 @@
-fn difs1(nums: &[i64]) -> i64 {
+fn difs(nums: &[i64]) -> i64 {
     if nums.iter().all(|n| *n == 0) {
         0
     } else {
-        let mut num = Vec::new();
-        for i in 0..(nums.len() - 1) {
-            num.push(nums[i+1] - nums[i]);
-        }
-        let r = num[num.len() - 1] + difs1(&num);
-        r
+        let diffs = nums
+            .iter()
+            .zip(nums.iter().skip(1))
+            .map(|(a, b)| b - a)
+            .collect::<Vec<_>>();
+
+        diffs.last().unwrap() + difs(&diffs)
     }
 }
 
 pub fn part_1(input: &str) -> impl std::fmt::Display {
-    input.lines().map(|l| {
-        let nums = l.split(' ').map(|n| n.parse::<i64>().unwrap()).collect::<Vec<_>>();
+    input
+        .lines()
+        .map(|l| {
+            let nums = l
+                .split(' ')
+                .map(|n| n.parse::<i64>().unwrap())
+                .collect::<Vec<_>>();
 
-        nums[nums.len() - 1] + difs1(&nums)
-    })
-    .sum::<i64>()
+            nums.last().unwrap() + difs(&nums)
+        })
+        .sum::<i64>()
 }
 
 pub fn part_2(input: &str) -> impl std::fmt::Display {
-    input.lines().map(|l| {
-        let nums = l.split(' ').map(|n| n.parse::<i64>().unwrap()).rev().collect::<Vec<_>>();
+    input
+        .lines()
+        .map(|l| {
+            let nums = l
+                .split(' ')
+                .map(|n| n.parse::<i64>().unwrap())
+                .rev()
+                .collect::<Vec<_>>();
 
-        nums[nums.len() - 1] + difs1(&nums)
-    })
-    .sum::<i64>()
+            nums.last().unwrap() + difs(&nums)
+        })
+        .sum::<i64>()
 }
