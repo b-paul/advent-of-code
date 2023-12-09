55b6a2bc3e0c229a467714b65fd030849d2087a7
Benjamin Paul <bpaul@bpaul.xyz>
2023-12-10 06:23:49 +1000

    Add uglier interpolation version which uses no split (for a 4x speedup !?!?)

--- src/year_2023/day9.rs
+++ src/year_2023/day9.rs
@@ -42,6 +42,36 @@ pub(crate) fn part_1_interpolation(input: &str) -> impl std::fmt::Display {
         .sum::<i64>()
 }
 
+pub(crate) fn part_1_interpolation_ugly(input: &str) -> impl std::fmt::Display {
+    const INTERP: [i64; 21] = [
+        1, -21, 210, -1330, 5985, -20349, 54264, -116280, 203490, -293930, 352716, -352716, 293930,
+        -203490, 116280, -54264, 20349, -5985, 1330, -210, 21,
+    ];
+    let mut sum = 0;
+    let mut i = 0;
+    let mut cur = 0;
+    let mut sign = 1;
+    for byte in input.bytes() {
+        if byte == b'\n' {
+            sum += cur * sign * INTERP[i];
+            sign = 1;
+            cur = 0;
+            i = 0;
+        } else if byte == b' ' {
+            sum += cur * sign * INTERP[i];
+            sign = 1;
+            cur = 0;
+            i += 1;
+        } else if byte == b'-' {
+            sign = -1;
+        } else {
+            cur *= 10;
+            cur += (byte - b'0') as i64;
+        }
+    }
+    sum
+}
+
 pub fn part_2(input: &str) -> impl std::fmt::Display {
     input
         .lines()
@@ -73,6 +103,36 @@ pub(crate) fn part_2_interpolation(input: &str) -> impl std::fmt::Display {
         .sum::<i64>()
 }
 
+pub(crate) fn part_2_interpolation_ugly(input: &str) -> impl std::fmt::Display {
+    const INTERP: [i64; 21] = [
+        21, -210, 1330, -5985, 20349, -54264, 116280, -203490, 293930, -352716, 352716, -293930,
+        203490, -116280, 54264, -20349, 5985, -1330, 210, -21, 1,
+    ];
+    let mut sum = 0;
+    let mut i = 0;
+    let mut cur = 0;
+    let mut sign = 1;
+    for byte in input.bytes() {
+        if byte == b'\n' {
+            sum += cur * sign * INTERP[i];
+            sign = 1;
+            cur = 0;
+            i = 0;
+        } else if byte == b' ' {
+            sum += cur * sign * INTERP[i];
+            sign = 1;
+            cur = 0;
+            i += 1;
+        } else if byte == b'-' {
+            sign = -1;
+        } else {
+            cur *= 10;
+            cur += (byte - b'0') as i64;
+        }
+    }
+    sum
+}
+
 #[cfg(test)]
 mod benches {
     use crate::get_input;
@@ -100,6 +160,18 @@ mod benches {
     }
 
     #[bench]
+    fn part1_interpolation_ugly(b: &mut Bencher) {
+        let input = &get_input(2023, 9).unwrap();
+        assert_eq!(
+            part_1_interpolation_ugly(input).to_string(),
+            part_1(input).to_string()
+        );
+        b.iter(|| {
+            black_box(part_1_interpolation_ugly(input));
+        })
+    }
+
+    #[bench]
     fn part2_normal(b: &mut Bencher) {
         let input = &get_input(2023, 9).unwrap();
         b.iter(|| {
@@ -116,6 +188,18 @@ mod benches {
         );
         b.iter(|| {
             black_box(part_2_interpolation(input));
+        })
+    }
+
+    #[bench]
+    fn part2_interpolation_ugly(b: &mut Bencher) {
+        let input = &get_input(2023, 9).unwrap();
+        assert_eq!(
+            part_2_interpolation_ugly(input).to_string(),
+            part_2(input).to_string()
+        );
+        b.iter(|| {
+            black_box(part_2_interpolation_ugly(input));
         })
     }
 }
