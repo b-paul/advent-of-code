75593cbe665a43950810ef92b8b62d1b767d91cc
bpaul <bpaul@bpaul.xyz>
2023-12-09 15:23:33 +1000

    day 9 yay recursion

--- src/year_2023/day9.rs
+++ src/year_2023/day9.rs
@@ -1,7 +1,30 @@
-pub fn part_1(_input: &str) -> impl std::fmt::Display {
-    "todo"
+fn difs1(nums: &[i64]) -> i64 {
+    if nums.iter().all(|n| *n == 0) {
+        0
+    } else {
+        let mut num = Vec::new();
+        for i in 0..(nums.len() - 1) {
+            num.push(nums[i+1] - nums[i]);
+        }
+        let r = num[num.len() - 1] + difs1(&num);
+        r
+    }
 }
 
-pub fn part_2(_input: &str) -> impl std::fmt::Display {
-    "todo"
+pub fn part_1(input: &str) -> impl std::fmt::Display {
+    input.lines().map(|l| {
+        let nums = l.split(' ').map(|n| n.parse::<i64>().unwrap()).collect::<Vec<_>>();
+
+        nums[nums.len() - 1] + difs1(&nums)
+    })
+    .sum::<i64>()
+}
+
+pub fn part_2(input: &str) -> impl std::fmt::Display {
+    input.lines().map(|l| {
+        let nums = l.split(' ').map(|n| n.parse::<i64>().unwrap()).rev().collect::<Vec<_>>();
+
+        nums[nums.len() - 1] + difs1(&nums)
+    })
+    .sum::<i64>()
 }
