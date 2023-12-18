b517fe7f2406396e4fd8960b96f994c71fa831d9
Benjamin Paul <bpaul@bpaul.xyz>
2023-12-18 21:22:13 +1000

    compute area as we parse 2023day18

--- src/year_2023/day18.rs
+++ src/year_2023/day18.rs
@@ -3,15 +3,12 @@ use itertools::Itertools;
 use crate::helper::prelude::*;
 
 pub fn part_1(input: &str) -> impl std::fmt::Display {
-    let size = (1000, 1000);
-
-    let mut grid = Grid::new_filled('.', size.0, size.1);
-
-    let mut pos = (size.0 / 2, size.1 / 2);
+    let mut cur = (0, 0);
+    let mut area = 0;
 
     for line in input.lines() {
         let is = line.split(' ').collect_vec();
-        let (dir, count, _) = (is[0], is[1].parse::<usize>().unwrap(), is[2]);
+        let (dir, count, _) = (is[0], is[1].parse::<isize>().unwrap(), is[2]);
         let dir = match dir.chars().next().unwrap() {
             'R' => Direction4::Right,
             'L' => Direction4::Left,
@@ -19,44 +16,27 @@ pub fn part_1(input: &str) -> impl std::fmt::Display {
             'D' => Direction4::Down,
             d => panic!("Invalid direction {d}"),
         };
-        for _ in 0..count {
-            grid[pos] = '#';
-            pos = dir.moveub(pos, size).unwrap();
-            grid[pos] = '#';
-        }
+
+        let next = dir.moveic(cur, count);
+
+        // shoelace formula
+        area += cur.0 * next.1 - next.0 * cur.1;
+
+        // Add the edges
+        area += cur.0.abs_diff(next.0) as isize;
+        area += cur.1.abs_diff(next.1) as isize;
+
+        cur = next;
     }
 
-    grid.floodfill_4('.', '8', (0, 0));
-
-    grid.iter_idx().filter(|(_, &c)| c != '8').count()
-}
-
-#[test]
-fn test() {
-    let input = "R 6 (#70c710)
-D 5 (#0dc571)
-L 2 (#5713f0)
-D 2 (#d2c081)
-R 2 (#59c680)
-D 2 (#411b91)
-L 5 (#8ceee2)
-U 2 (#caa173)
-L 1 (#1b58a2)
-U 2 (#caa171)
-R 2 (#7807d2)
-U 3 (#a77fa3)
-L 2 (#015232)
-U 2 (#7a21e3)
-";
-    let output = 952408144115usize;
-    assert_eq!(part_2(input).to_string(), output.to_string());
+    // abs diff misses one corner, but then the edges get joined together so only one corner is
+    // missed overall (i think ?!?!)
+    area / 2 + 1
 }
 
 pub fn part_2(input: &str) -> impl std::fmt::Display {
-    let mut vertices = Vec::new();
-
     let mut cur = (0, 0);
-    vertices.push(cur);
+    let mut area = 0;
 
     for line in input.lines() {
         let is = line.split(' ').collect_vec();
@@ -69,30 +49,49 @@ pub fn part_2(input: &str) -> impl std::fmt::Display {
             '1' => Direction4::Down,
             d => panic!("Invalid direction {d}"),
         };
-        let mut colour = 0;
+        let mut count = 0;
         let col = &col[2..7];
         for c in col.chars() {
-            colour *= 16;
-            colour += c.to_digit(16).unwrap() as isize;
+            count *= 16;
+            count += c.to_digit(16).unwrap() as isize;
         }
 
-        cur = dir.moveic(cur, colour);
+        let next = dir.moveic(cur, count);
 
-        vertices.push(cur);
-    }
-
-    let mut area = 0;
-    let n = vertices.len();
-    for i in 0..vertices.len() {
-        let ip = (n + i + 1) % n;
         // shoelace formula
-        area += vertices[i].0 * vertices[ip].1 - vertices[ip].0 * vertices[i].1;
+        area += cur.0 * next.1 - next.0 * cur.1;
 
         // Add the edges
-        area += vertices[i].0.abs_diff(vertices[ip].0) as isize;
-        area += vertices[i].1.abs_diff(vertices[ip].1) as isize;
+        area += cur.0.abs_diff(next.0) as isize;
+        area += cur.1.abs_diff(next.1) as isize;
+
+        cur = next;
     }
+
     // abs diff misses one corner, but then the edges get joined together so only one corner is
     // missed overall (i think ?!?!)
     area / 2 + 1
+}
+
+#[cfg(test)]
+mod benches {
+    use crate::get_input;
+    use crate::year_2023::day18::*;
+    use test::{black_box, Bencher};
+
+    #[bench]
+    fn part1_normal(b: &mut Bencher) {
+        let input = &get_input(2023, 18).unwrap();
+        b.iter(|| {
+            black_box(part_1(input));
+        })
+    }
+
+    #[bench]
+    fn part2_normal(b: &mut Bencher) {
+        let input = &get_input(2023, 18).unwrap();
+        b.iter(|| {
+            black_box(part_2(input));
+        })
+    }
 }
