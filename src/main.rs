d335ee5c67558f074108a9fc2ffa29e9e81fa7b2
bpaul <bpaul@bpaul.xyz>
2024-11-30 15:33:57 +1000

    Do a 2017 problem for practice

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,5 @@
 #![allow(dead_code)]
 #![allow(unused_imports)]
-#![feature(slice_group_by)]
-#![feature(exclusive_range_pattern)]
 #![feature(iter_array_chunks)]
 #![feature(array_chunks)]
 #![feature(test)]
