f806537f54459dfb9efeb15b4806621d9d6eb80b
bpaul <bpaul@bpaul.xyz>
2024-12-05 15:21:15 +1000

    Helper idea in todo

--- src/helper/mod.rs
+++ src/helper/mod.rs
@@ -1,4 +1,4 @@
-// TODO graph algos, range arithmetic, 3d grid ?!, counter
+// TODO graph algos, range arithmetic, 3d grid ?!, counter, a -> P(b) hashmap
 pub mod adjacency;
 pub mod grid;
 pub mod point;
