a38ab67ecf2f55a501ee34f76e0654a05cb7749a
bpaul <bpaul@bpaul.xyz>
2024-12-07 02:16:02 +1000

    benchmark + optimisation

--- src/year_2024/day6.rs
+++ src/year_2024/day6.rs
@@ -42,8 +42,28 @@ fn test() {
 
 pub fn part_2(input: &str) -> impl std::fmt::Display {
     let grid = input.parse::<Grid<char>>().unwrap();
-    grid.iter_idx()
-        .filter(|(pos, _)| {
+    let squares = {
+        let mut squares = HashSet::new();
+
+        let mut pos = grid.find(&'^').unwrap();
+        let mut dir = Direction4::Up;
+
+        loop {
+            squares.insert(pos);
+            let Some(posp) = dir.moveub(pos, (grid.width(), grid.height())) else {
+                break;
+            };
+            if grid.get(posp) == Some(&'#') {
+                dir = dir.cw();
+            } else {
+                pos = posp;
+            }
+        }
+
+        squares
+    };
+    squares.into_iter()
+        .filter(|pos| {
             let mut grid = grid.clone();
             grid[*pos] = '#';
             let mut squares = HashSet::new();
@@ -69,4 +89,27 @@ pub fn part_2(input: &str) -> impl std::fmt::Display {
             }
         })
         .count()
+}
+
+#[cfg(test)]
+mod benches {
+    use crate::get_input;
+    use crate::year_2024::day6::*;
+    use test::{black_box, Bencher};
+
+    #[bench]
+    fn part1_normal(b: &mut Bencher) {
+        let input = &get_input(2024, 6).unwrap();
+        b.iter(|| {
+            black_box(part_1(input));
+        })
+    }
+
+    #[bench]
+    fn part2_normal(b: &mut Bencher) {
+        let input = &get_input(2024, 6).unwrap();
+        b.iter(|| {
+            black_box(part_2(input));
+        })
+    }
 }
