fe9ecc90257077bc72bf2c28bcc0f3696aa62678
bpaul <bpaul@bpaul.xyz>
2024-12-06 15:16:10 +1000

    Grid lib carried me

--- src/year_2024/day6.rs
+++ src/year_2024/day6.rs
@@ -1,7 +1,72 @@
-pub fn part_1(_input: &str) -> impl std::fmt::Display {
-    "todo"
+use crate::helper::prelude::*;
+use itertools::Itertools;
+use std::collections::*;
+
+pub fn part_1(input: &str) -> impl std::fmt::Display {
+    let grid = input.parse::<Grid<char>>().unwrap();
+    let mut squares = HashSet::new();
+
+    let mut pos = grid.find(&'^').unwrap();
+    let mut dir = Direction4::Up;
+
+    loop {
+        squares.insert(pos);
+        let Some(posp) = dir.moveub(pos, (grid.width(), grid.height())) else {
+            break;
+        };
+        if grid.get(posp) == Some(&'#') {
+            dir = dir.cw();
+        } else {
+            pos = posp;
+        }
+    }
+
+    squares.len()
 }
 
-pub fn part_2(_input: &str) -> impl std::fmt::Display {
-    "todo"
+#[test]
+fn test() {
+    let input = "....#.....
+.........#
+..........
+..#.......
+.......#..
+..........
+.#..^.....
+........#.
+#.........
+......#...";
+    let output = 6;
+    assert_eq!(part_2(input).to_string(), output.to_string());
+}
+
+pub fn part_2(input: &str) -> impl std::fmt::Display {
+    let grid = input.parse::<Grid<char>>().unwrap();
+    grid.iter_idx()
+        .filter(|(pos, _)| {
+            let mut grid = grid.clone();
+            grid[*pos] = '#';
+            let mut squares = HashSet::new();
+
+            let Some(mut pos) = grid.find(&'^') else {
+                return false;
+            };
+            let mut dir = Direction4::Up;
+
+            loop {
+                if squares.contains(&(pos, dir)) {
+                    return true;
+                }
+                squares.insert((pos, dir));
+                let Some(posp) = dir.moveub(pos, (grid.width(), grid.height())) else {
+                    return false;
+                };
+                if grid.get(posp) == Some(&'#') {
+                    dir = dir.cw();
+                } else {
+                    pos = posp;
+                }
+            }
+        })
+        .count()
 }
