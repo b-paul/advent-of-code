7a48e626f9251ab6aa483167187e550d192d61eb
bpaul <bpaul@bpaul.xyz>
2024-12-08 19:27:38 +1000

    Faster algorithm

--- src/year_2024/day8.rs
+++ src/year_2024/day8.rs
@@ -7,26 +7,29 @@ pub fn part_1(input: &str) -> impl std::fmt::Display {
 
     let mut antinodes = grid.clone().map(|_| false);
 
-    // Potential optimistions:
-    // For each frequency, store a list of points on the grid that the frequency exists at.
-    // This way, when we do this n^2 loop, n is much much smaller.
+    // TODO perhaps this kind of structure could be a helper with a FromIter impl
 
-    for (a, b) in grid
-        .iter_idx()
-        .cartesian_product(grid.iter_idx())
-        .filter_map(|((a, c1), (b, c2))| {
-            (a != b && c1.is_alphanumeric() && c1 == c2).then_some((a, b))
-        })
-    {
-        let d = a.rel_off(b);
-        // We do the other direction when doing the (b, a) step
-        if let Some(p) = a.move_off(d) {
-            if antinodes.contains_point(p) {
-                antinodes[p] = true;
+    let mut point_lists = HashMap::<_, Vec<_>>::new();
+
+    for (p, v) in grid.iter_idx() {
+        if v.is_alphanumeric() {
+            point_lists.entry(*v).or_default().push(p);
+        }
+    }
+
+    for v in point_lists.values() {
+        for (&a, &b) in v.iter().cartesian_product(v.iter()).filter(|(a, b)| a != b) {
+            let d = a.rel_off(b);
+            // We do the other direction when doing the (b, a) step
+            if let Some(p) = a.move_off(d) {
+                if antinodes.contains_point(p) {
+                    antinodes[p] = true;
+                }
             }
         }
     }
 
+    /*
     println!(
         "{}",
         antinodes.clone().map(|b| match b {
@@ -34,6 +37,7 @@ pub fn part_1(input: &str) -> impl std::fmt::Display {
             false => '.',
         })
     );
+    */
 
     antinodes.iter_idx().filter(|(_, &b)| b).count()
 }
@@ -61,24 +65,29 @@ pub fn part_2(input: &str) -> impl std::fmt::Display {
 
     let mut antinodes = grid.clone().map(|_| false);
 
-    for (a, b) in grid
-        .iter_idx()
-        .cartesian_product(grid.iter_idx())
-        .filter_map(|((a, c1), (b, c2))| {
-            (a != b && c1.is_alphanumeric() && c1 == c2).then_some((a, b))
-        })
-    {
-        let d = a.rel_off(b);
-        let mut p = a;
-        while antinodes.contains_point(p) {
-            antinodes[p] = true;
-            p = match p.move_off(d) {
-                Some(p) => p,
-                None => break,
-            };
+    let mut point_lists = HashMap::<_, Vec<_>>::new();
+
+    for (p, v) in grid.iter_idx() {
+        if v.is_alphanumeric() {
+            point_lists.entry(*v).or_default().push(p);
         }
     }
 
+    for v in point_lists.values() {
+        for (&a, &b) in v.iter().cartesian_product(v.iter()).filter(|(a, b)| a != b) {
+            let d = a.rel_off(b);
+            let mut p = a;
+            while antinodes.contains_point(p) {
+                antinodes[p] = true;
+                p = match p.move_off(d) {
+                    Some(p) => p,
+                    None => break,
+                };
+            }
+        }
+    }
+
+    /*
     println!(
         "{}",
         antinodes.clone().map(|b| match b {
@@ -86,6 +95,7 @@ pub fn part_2(input: &str) -> impl std::fmt::Display {
             false => '.',
         })
     );
+    */
 
     antinodes.iter_idx().filter(|(_, &b)| b).count()
 }
