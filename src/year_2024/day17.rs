b9c1ab1667430e5bdcc881bb37f1bcbfec68db9d
bpaul <bpaul@bpaul.xyz>
2024-12-18 11:27:42 +1000

    benches

--- src/year_2024/day17.rs
+++ src/year_2024/day17.rs
@@ -153,3 +153,26 @@ pub fn part_2(input: &str) -> impl std::fmt::Display {
 
     0
 }
+
+#[cfg(test)]
+mod benches {
+    use crate::get_input;
+    use crate::year_2024::day17::*;
+    use test::{black_box, Bencher};
+
+    #[bench]
+    fn part1_normal(b: &mut Bencher) {
+        let input = &get_input(2024, 17).unwrap();
+        b.iter(|| {
+            black_box(part_1(input));
+        })
+    }
+
+    #[bench]
+    fn part2_normal(b: &mut Bencher) {
+        let input = &get_input(2024, 17).unwrap();
+        b.iter(|| {
+            black_box(part_2(input));
+        })
+    }
+}
