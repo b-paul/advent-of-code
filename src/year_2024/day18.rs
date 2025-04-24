c87ede3999891bd3b1efe221c4ff06b46713aee7
Benjamin Paul <bpaul@bpaul.xyz>
2025-04-24 20:53:04 +1000

    Bitwise search for 2024 day18 p1

--- src/year_2024/day18.rs
+++ src/year_2024/day18.rs
@@ -23,6 +23,55 @@ pub fn part_1(input: &str) -> impl std::fmt::Display {
     best_score.unwrap()
 }
 
+#[allow(unused)]
+fn print_board(occ: [u128; 71], visited: [u128; 71]) {
+    for y in 0..=70 {
+        for x in 0..=70 {
+            if occ[y] & (1 << x) == 0 {
+                print!("O");
+            } else if visited[y] & (1 << x) != 0 {
+                print!("#");
+            } else {
+                print!(".");
+            }
+        }
+        println!();
+    }
+    println!();
+    println!();
+}
+
+pub fn part_1_faster(input: &str) -> impl std::fmt::Display {
+    const ROW_MASK: u128 = (1 << 71) - 1;
+    let mut occ = [!0u128; 71];
+    let mut visited = [0u128; 71];
+    let mut visited2 = [0u128; 71];
+
+    for p in input.lines().map(read_point).take(1024) {
+        occ[p.y as usize] &= !(1 << p.x);
+    }
+
+    let mut count = 0;
+    visited[0] = 1;
+    while visited[70] & (1 << 70) == 0 {
+        count += 1;
+
+        for row in 0..=70 {
+            visited2[row] = (visited[row] | visited[row] >> 1 | visited[row] << 1) & ROW_MASK;
+            if row != 0 {
+                visited2[row] |= visited[row - 1]
+            }
+            if row != 70 {
+                visited2[row] |= visited[row + 1]
+            }
+            visited2[row] &= occ[row];
+        }
+        visited = visited2;
+    }
+
+    count
+}
+
 #[test]
 fn test() {
     let input = "5,4
@@ -67,10 +116,34 @@ pub fn part_2(input: &str) -> impl std::fmt::Display {
         let mut ok = false;
         grid.bfs_4(bounds.top_left(), |p, _, _| ok |= p == exit, |_, _, to| !to);
         if !ok {
-            println!("{}", points[d]);
-            return "manual submit today :(".to_string();
+            let p = points[d];
+            return format!("{},{}", p.x, p.y);
         }
     }
 
-    "manual submit today :(".to_string()
+    panic!()
+}
+
+#[cfg(test)]
+mod benches {
+    use crate::get_input;
+    use crate::year_2024::day18::*;
+    use test::{black_box, Bencher};
+
+    #[bench]
+    fn part1_normal(b: &mut Bencher) {
+        let input = &get_input(2024, 18).unwrap();
+        b.iter(|| {
+            black_box(part_1(input));
+        })
+    }
+
+    #[bench]
+    fn part1_faster(b: &mut Bencher) {
+        let input = &get_input(2024, 18).unwrap();
+        assert_eq!(part_1_faster(input).to_string(), part_1(input).to_string());
+        b.iter(|| {
+            black_box(part_1_faster(input));
+        })
+    }
 }
