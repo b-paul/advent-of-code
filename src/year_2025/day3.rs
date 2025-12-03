b5f97f3d3a2eaf181c4eb4f5782df0bde3b4ac5c
bpaul <bpaul@bpaul.xyz>
2025-12-03 18:38:46 +1000

    Day 3 yay

--- src/year_2025/day3.rs
+++ src/year_2025/day3.rs
@@ -1,7 +1,30 @@
-pub fn part_1(_input: &str) -> impl std::fmt::Display {
-    "todo"
+fn solve<const N: usize>(input: &str) -> u64 {
+    input
+        .lines()
+        .map(|s| {
+            s.chars()
+                .map(|c| c.to_digit(10).unwrap() as u64)
+                .map_windows::<_, _, N>(|v| v.to_vec())
+                .reduce(|m, mut w| {
+                    // how do i functionalise this ?!?!
+                    for i in 0..m.len() {
+                        if m[i] < w[i] {
+                            break;
+                        }
+                        w[i] = m[i];
+                    }
+                    w
+                })
+                .map(|m| m.into_iter().fold(0, |a, b| 10 * a + b))
+                .unwrap()
+        })
+        .sum::<u64>()
 }
 
-pub fn part_2(_input: &str) -> impl std::fmt::Display {
-    "todo"
+pub fn part_1(input: &str) -> impl std::fmt::Display {
+    solve::<2>(input)
+}
+
+pub fn part_2(input: &str) -> impl std::fmt::Display {
+    solve::<12>(input)
 }
