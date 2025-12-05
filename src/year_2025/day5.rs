55a25c8829de5f24d8f4cf3c540d642767d0da27
bpaul <bpaul@bpaul.xyz>
2025-12-05 16:20:11 +1000

    day 5

--- src/year_2025/day5.rs
+++ src/year_2025/day5.rs
@@ -1,7 +1,56 @@
-pub fn part_1(_input: &str) -> impl std::fmt::Display {
-    "todo"
+use crate::helper::prelude::*;
+use itertools::Itertools;
+use std::collections::*;
+
+pub fn part_1(input: &str) -> impl std::fmt::Display {
+    let mut input = input.split("\n\n");
+
+    let rs = input
+        .next()
+        .unwrap()
+        .lines()
+        .map(|s| {
+            let (l, h) = s.split_once('-').unwrap();
+            p::<u64>(l)..=p::<u64>(h)
+        })
+        .collect_vec();
+
+    input
+        .next()
+        .unwrap()
+        .lines()
+        .filter(|&n| {
+            let n = p::<u64>(n);
+            rs.iter().any(|r| r.contains(&n))
+        })
+        .count()
 }
 
-pub fn part_2(_input: &str) -> impl std::fmt::Display {
-    "todo"
+pub fn part_2(input: &str) -> impl std::fmt::Display {
+    let mut input = input.split("\n\n");
+
+    let mut rs = input
+        .next()
+        .unwrap()
+        .lines()
+        .map(|s| {
+            let (l, h) = s.split_once('-').unwrap();
+            p::<u64>(l)..p::<u64>(h)
+        })
+        .collect_vec();
+    rs.sort_by_key(|a| a.start);
+
+    let mut i = 1;
+    while i < rs.len() {
+        if rs[i - 1].end >= rs[i].start {
+            rs[i] = rs[i - 1].end + 1..rs[i].end;
+        }
+        if rs[i].end < rs[i].start {
+            rs.remove(i);
+        } else {
+            i += 1;
+        }
+    }
+
+    rs.into_iter().map(|r| r.end - r.start + 1).sum::<u64>()
 }
